56113b2baac7014c3e7ceb6aa8804bdc649c5b12
buttegab <gabriel.butterick@students.olin.edu>
2017-01-29 14:07:58 -0500

    exercises

---
+++ exercises/ex01/hello.s
@@ -0,0 +1,16 @@
+	.file	"hello.c"
+	.section	.text.startup,"ax",@progbits
+	.p2align 4,,15
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	movl	$6, %edi
+	xorl	%eax, %eax
+	jmp	printf
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4"
+	.section	.note.GNU-stack,"",@progbits
